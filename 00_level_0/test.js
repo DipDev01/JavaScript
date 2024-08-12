34ba1f2b9ba122749d57f297e923a7f7b154ff61
DipDev01 <devkotadipesh14@gmail.com>
2024-08-12 18:05:33 +0000

    container config done

--- 00_level_0/test.js
+++ 00_level_0/test.js
@@ -1,1 +1,39 @@
 console.log("This will be Amazing")
+
+// This is it learned to print on the console
+// Next Step to build E-Commerce platform :)
+
+
+// Let's see how to do them , First there is something called const, let, var. What are these though
+// Let's try it
+
+const platformName = "pauchu"
+let platformEmail = "pauchu123@gmail.com"
+var contact = 9834892930
+
+// From what i have know there seems to be assignment of these variables something like Declaration and Initialization
+
+// In line 10 "const" is the keyword and "platformName" is the variable where we assigned "pauchu" as value
+// There seems to be 3 keywords to declare variable for now at least :)
+
+// Let'sprint in console
+
+console.log(platformName) //Output is pauchu
+console.log(platformEmail) //Output is pauchu123@gmail.com
+console.log(contact)  //Output is 9834892930
+
+
+//So what can we do more let's dive deeper
+
+// In programming there is some concepts know as data type 
+// Seems to be annoying but it has it's own aspect like
+
+// String can hold anytype of data i.e. @,4,a,""  Numbers , alphabets, and special charecters
+// integer can hold numbers
+// boolean can hold true,false
+// char can hold A,B,c,d any charecter
+
+// So how does these things differ and what are the use case of these things?
+
+// Well it may look similar to one another but every data type has it's own meaning and everything was builded on too many concepts
+// We will explore them as time passes
