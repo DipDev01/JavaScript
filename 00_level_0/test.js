fe91b91a14fac2108f0bb56b5eeba1e9413bf561
DipDev01 <devkotadipesh14@gmail.com>
2024-08-14 07:11:41 +0000

    test completed

--- 00_level_0/test.js
+++ 00_level_0/test.js
@@ -11,12 +11,12 @@ const platformName = "pauchu"
 let platformEmail = "pauchu123@gmail.com"
 var contact = 9834892930
 
-// From what i have know there seems to be assignment of these variables something like Declaration and Initialization
+// // From what i have know there seems to be assignment of these variables something like Declaration and Initialization
 
-// In line 10 "const" is the keyword and "platformName" is the variable where we assigned "pauchu" as value
-// There seems to be 3 keywords to declare variable for now at least :)
+// // In line 10 "const" is the keyword and "platformName" is the variable where we assigned "pauchu" as value
+// // There seems to be 3 keywords to declare variable for now at least :)
 
-// Let'sprint in console
+// // Let'sprint in console
 
 console.log(platformName) //Output is pauchu
 console.log(platformEmail) //Output is pauchu123@gmail.com
@@ -29,11 +29,36 @@ console.log(contact)  //Output is 9834892930
 // Seems to be annoying but it has it's own aspect like
 
 // String can hold anytype of data i.e. @,4,a,""  Numbers , alphabets, and special charecters
-// integer can hold numbers
+// Number can hold 1,2,3,4 etc witht decimal and floats too
 // boolean can hold true,false
-// char can hold A,B,c,d any charecter
+// undefined is when we print var that has not been initialized
+let email;
+console.log(email);
 
-// So how does these things differ and what are the use case of these things?
+let z=undefined = "hello";
+console.info(z); //looks like we can initialize undefined
 
-// Well it may look similar to one another but every data type has it's own meaning and everything was builded on too many concepts
+
+// let x = null = "Hello" //null cannot be intialized
+// consolek.log(x)
+
+// lets try something else\
+
+
+console.log(undefined>null) //false
+console.log(null>undefined) //false
+console.log(null==undefined) //It seems to be true
+console.log(typeof(null)); //object
+console.log(typeof(undefined)); // undefined
+console.log(+(undefined)); // NaN
+console.log(+(null)); // 0
+
+// So it seems that undefined is itself type undefined byt null is object type
+// There is something called NaN which means Not a Number 
+
+// null is something that comes aft
+// So how does datatype differ and what are the use case of these things?
+
+// Well it may look similar to one another but every data type has it's own meaning and everything was builded on many concepts
 // We will explore them as time passes
+// I want to dive into projects basics sucks :(
